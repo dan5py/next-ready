4cc606c77bdeebc8bc101a12a617ec8deffbeb85
Daniele Luisetto <59441403+dan5py@users.noreply.github.com>
2023-12-18 16:12:17 +0100

    chore: add husky format check (#1)
    * feat(lint): add lint-staged with husky
    
    * refactor: add prettier plugin for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': 'error',
   },
 };
